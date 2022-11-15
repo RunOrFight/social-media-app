043652067ddcb22556755b298acced7cf4fd5334
RunOrFight <dragoinfury@gmail.com>
2022-11-15 22:39:22 +0300

    Completed the initial app setup

--- api/index.js
+++ api/index.js
@@ -1,5 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
 const app = express();
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {}, () => {
+  console.log("Connected to MongoDB");
+});
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
 
 app.listen(3000, () => {
   console.log("Server is running");
