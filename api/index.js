a67747131472d24742ee7f7415bb5517f7acf0f9
RunOrFight <dragoinfury@gmail.com>
2022-11-16 06:35:37 +0300

    App using routes

--- api/index.js
+++ api/index.js
@@ -12,9 +12,15 @@ mongoose.connect(process.env.MONGO_URL, {}, () => {
   console.log("Connected to MongoDB");
 });
 
+const usersRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+app.use("/api/users", usersRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(3000, () => {
   console.log("Server is running");
