659de410e508a2301d1a042ea40109c03df76264
RunOrFight <dragoinfury@gmail.com>
2022-11-17 23:28:47 +0300

    Rebuilded topbar

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -8,37 +8,43 @@ import {
 const Topbar = () => {
   return (
     <div className={cls.container}>
-      <div className={cls.logo}>My Social</div>
-      <div className={cls.searchbar}>
-        <SearchSharp />
-        <input
-          type='text'
-          placeholder='Search for friends, posts or video'
+      <div className={cls.left}>
+        <div className={cls.logo}>My Social</div>
+        <div className={cls.links}>
+          <span className={cls.link}>Homepage</span>
+          <span className={cls.link}>Timeline</span>
+        </div>
+      </div>
+      <div className={cls.center}>
+        <div className={cls.searchbar}>
+          <SearchSharp />
+          <input
+            type='text'
+            placeholder='Search for friends, posts or video'
+          />
+        </div>
+      </div>
+      <div className={cls.right}>
+        <div className={cls.icons}>
+          <div className={cls.icon}>
+            <PersonSharp />
+            <span className={cls.badge}>9</span>
+          </div>
+          <div className={cls.icon}>
+            <ChatSharp />
+            <span className={cls.badge}>5</span>
+          </div>
+          <div className={cls.icon}>
+            <NotificationsSharp />
+            <span className={cls.badge}>9</span>
+          </div>
+        </div>
+        <img
+          className={cls.user_image}
+          src='/assets/users/sasha.jpeg'
+          alt='Profile'
         />
       </div>
-      <div className={cls.links}>
-        <span className={cls.link}>Homepage</span>
-        <span className={cls.link}>Timeline</span>
-      </div>
-      <div className={cls.icons}>
-        <div className={cls.icon}>
-          <PersonSharp />
-          <span className={cls.badge}>9</span>
-        </div>
-        <div className={cls.icon}>
-          <ChatSharp />
-          <span className={cls.badge}>5</span>
-        </div>
-        <div className={cls.icon}>
-          <NotificationsSharp />
-          <span className={cls.badge}>9</span>
-        </div>
-      </div>
-      <img
-        className={cls.user_image}
-        src='/assets/users/sasha.jpeg'
-        alt='Profile'
-      />
     </div>
   );
 };
